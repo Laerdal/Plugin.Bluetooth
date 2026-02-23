bff5e6c2e28a29a3191d294a908933bf64fd0dfd
Francois Raminosona <francois.raminosona@laerdal.com>
2026-02-23 17:13:51 +0100

    feat (platforms): add comprehensive GATT mapping for iOS and Android
    Reintroduces and expands Bluetooth characteristic and descriptor property and permission mappings for both Apple and Android platforms to enable reliable bidirectional conversions between shared and native types.

--- Bluetooth.Maui.Platforms.Apple/Tools/GattMapping.cs
+++ Bluetooth.Maui.Platforms.Apple/Tools/GattMapping.cs
@@ -2,9 +2,12 @@ namespace Bluetooth.Maui.Platforms.Apple.Tools;
 
 /// <summary>
 ///     Extension methods for converting between Bluetooth characteristic properties and permissions to their iOS CoreBluetooth equivalents.
+///     Supports bidirectional conversion between abstract types and native Apple types.
 /// </summary>
 public static class GattMapping
 {
+    #region Properties - Shared to Native
+
     /// <summary>
     ///     Converts the specified <see cref="BluetoothCharacteristicProperties" /> to the corresponding iOS <see cref="CBCharacteristicProperties" /> flags.
     /// </summary>
@@ -55,6 +58,64 @@ public static class GattMapping
         return result;
     }
 
+    #endregion
+
+    #region Properties - Native to Shared
+
+    /// <summary>
+    ///     Converts iOS <see cref="CBCharacteristicProperties" /> flags to the corresponding <see cref="BluetoothCharacteristicProperties" />.
+    /// </summary>
+    /// <param name="nativeProps">The iOS CoreBluetooth characteristic property flags to convert.</param>
+    /// <returns>A <see cref="BluetoothCharacteristicProperties" /> value representing the specified native properties.</returns>
+    public static BluetoothCharacteristicProperties ToSharedCharacteristicProperties(this CBCharacteristicProperties nativeProps)
+    {
+        BluetoothCharacteristicProperties result = 0;
+
+        if (nativeProps.HasFlag(CBCharacteristicProperties.Broadcast))
+        {
+            result |= BluetoothCharacteristicProperties.Broadcast;
+        }
+
+        if (nativeProps.HasFlag(CBCharacteristicProperties.Read))
+        {
+            result |= BluetoothCharacteristicProperties.Read;
+        }
+
+        if (nativeProps.HasFlag(CBCharacteristicProperties.WriteWithoutResponse))
+        {
+            result |= BluetoothCharacteristicProperties.WriteWithoutResponse;
+        }
+
+        if (nativeProps.HasFlag(CBCharacteristicProperties.Write))
+        {
+            result |= BluetoothCharacteristicProperties.Write;
+        }
+
+        if (nativeProps.HasFlag(CBCharacteristicProperties.Notify))
+        {
+            result |= BluetoothCharacteristicProperties.Notify;
+        }
+
+        if (nativeProps.HasFlag(CBCharacteristicProperties.Indicate))
+        {
+            result |= BluetoothCharacteristicProperties.Indicate;
+        }
+
+        if (nativeProps.HasFlag(CBCharacteristicProperties.ExtendedProperties))
+        {
+            result |= BluetoothCharacteristicProperties.ExtendedProperties;
+        }
+
+        // Note: CoreBluetooth AuthenticatedSignedWrites exist but aren't directly exposed in CBCharacteristicProperties
+        // SignedWrite would need special handling if present in CBCharacteristicProperties
+
+        return result;
+    }
+
+    #endregion
+
+    #region Characteristic Permissions - Shared to Native
+
     /// <summary>
     ///     Converts the specified <see cref="BluetoothCharacteristicPermissions" /> to the corresponding iOS <see cref="CBAttributePermissions" /> flags.
     /// </summary>
@@ -104,6 +165,50 @@ public static class GattMapping
         return result;
     }
 
+    #endregion
+
+    #region Characteristic Permissions - Native to Shared
+
+    /// <summary>
+    ///     Converts iOS <see cref="CBAttributePermissions" /> flags to the corresponding <see cref="BluetoothCharacteristicPermissions" />.
+    /// </summary>
+    /// <param name="nativePerms">The iOS CoreBluetooth attribute permission flags to convert.</param>
+    /// <returns>A <see cref="BluetoothCharacteristicPermissions" /> value representing the specified native permissions.</returns>
+    /// <remarks>
+    ///     CoreBluetooth's permission model is simpler than Android's, so some granularity is lost in reverse conversion.
+    ///     Encryption-required permissions are mapped to authenticated permissions for consistency.
+    /// </remarks>
+    public static BluetoothCharacteristicPermissions ToSharedCharacteristicPermissions(this CBAttributePermissions nativePerms)
+    {
+        BluetoothCharacteristicPermissions result = 0;
+
+        // Read permissions
+        if (nativePerms.HasFlag(CBAttributePermissions.ReadEncryptionRequired))
+        {
+            result |= BluetoothCharacteristicPermissions.ReadAuthenticated;
+        }
+        else if (nativePerms.HasFlag(CBAttributePermissions.Readable))
+        {
+            result |= BluetoothCharacteristicPermissions.Read;
+        }
+
+        // Write permissions
+        if (nativePerms.HasFlag(CBAttributePermissions.WriteEncryptionRequired))
+        {
+            result |= BluetoothCharacteristicPermissions.WriteAuthenticated;
+        }
+        else if (nativePerms.HasFlag(CBAttributePermissions.Writeable))
+        {
+            result |= BluetoothCharacteristicPermissions.Write;
+        }
+
+        return result;
+    }
+
+    #endregion
+
+    #region Descriptor Permissions - Shared to Native
+
     /// <summary>
     ///     Converts the specified <see cref="BluetoothDescriptorPermissions" /> to the corresponding iOS <see cref="CBAttributePermissions" /> flags.
     /// </summary>
@@ -137,4 +242,46 @@ public static class GattMapping
 
         return result;
     }
+
+    #endregion
+
+    #region Descriptor Permissions - Native to Shared
+
+    /// <summary>
+    ///     Converts iOS <see cref="CBAttributePermissions" /> flags to the corresponding <see cref="BluetoothDescriptorPermissions" />.
+    /// </summary>
+    /// <param name="nativePerms">The iOS CoreBluetooth attribute permission flags to convert.</param>
+    /// <returns>A <see cref="BluetoothDescriptorPermissions" /> value representing the specified native permissions.</returns>
+    /// <remarks>
+    ///     CoreBluetooth's permission model is simpler than Android's, so some granularity is lost in reverse conversion.
+    ///     Encryption-required permissions are mapped to authenticated permissions for consistency.
+    /// </remarks>
+    public static BluetoothDescriptorPermissions ToSharedDescriptorPermissions(this CBAttributePermissions nativePerms)
+    {
+        BluetoothDescriptorPermissions result = 0;
+
+        // Read permissions
+        if (nativePerms.HasFlag(CBAttributePermissions.ReadEncryptionRequired))
+        {
+            result |= BluetoothDescriptorPermissions.ReadAuthenticated;
+        }
+        else if (nativePerms.HasFlag(CBAttributePermissions.Readable))
+        {
+            result |= BluetoothDescriptorPermissions.Read;
+        }
+
+        // Write permissions
+        if (nativePerms.HasFlag(CBAttributePermissions.WriteEncryptionRequired))
+        {
+            result |= BluetoothDescriptorPermissions.WriteAuthenticated;
+        }
+        else if (nativePerms.HasFlag(CBAttributePermissions.Writeable))
+        {
+            result |= BluetoothDescriptorPermissions.Write;
+        }
+
+        return result;
+    }
+
+    #endregion
 }
--- Bluetooth.Maui.Platforms.Droid/Tools/GattMapping.cs
+++ Bluetooth.Maui.Platforms.Droid/Tools/GattMapping.cs
@@ -2,9 +2,12 @@ namespace Bluetooth.Maui.Platforms.Droid.Tools;
 
 /// <summary>
 ///     Provides extension methods for converting Bluetooth characteristic properties and permissions to their corresponding Android GATT flags.
+///     Supports bidirectional conversion between abstract types and native Android types.
 /// </summary>
 public static class GattMapping
 {
+    #region Properties - Shared to Native
+
     /// <summary>
     ///     Converts the specified <see cref="BluetoothCharacteristicProperties" /> to the corresponding Android <see cref="GattProperty" /> flags.
     /// </summary>
@@ -57,6 +60,66 @@ public static class GattMapping
         return result;
     }
 
+    #endregion
+
+    #region Properties - Native to Shared
+
+    /// <summary>
+    ///     Converts Android <see cref="GattProperty" /> flags to the corresponding <see cref="BluetoothCharacteristicProperties" />.
+    /// </summary>
+    /// <param name="nativeProps">The Android GATT property flags to convert.</param>
+    /// <returns>A <see cref="BluetoothCharacteristicProperties" /> value representing the specified native properties.</returns>
+    public static BluetoothCharacteristicProperties ToSharedCharacteristicProperties(this GattProperty nativeProps)
+    {
+        BluetoothCharacteristicProperties result = 0;
+
+        if (nativeProps.HasFlag(GattProperty.Broadcast))
+        {
+            result |= BluetoothCharacteristicProperties.Broadcast;
+        }
+
+        if (nativeProps.HasFlag(GattProperty.Read))
+        {
+            result |= BluetoothCharacteristicProperties.Read;
+        }
+
+        if (nativeProps.HasFlag(GattProperty.WriteNoResponse))
+        {
+            result |= BluetoothCharacteristicProperties.WriteWithoutResponse;
+        }
+
+        if (nativeProps.HasFlag(GattProperty.Write))
+        {
+            result |= BluetoothCharacteristicProperties.Write;
+        }
+
+        if (nativeProps.HasFlag(GattProperty.Notify))
+        {
+            result |= BluetoothCharacteristicProperties.Notify;
+        }
+
+        if (nativeProps.HasFlag(GattProperty.Indicate))
+        {
+            result |= BluetoothCharacteristicProperties.Indicate;
+        }
+
+        if (nativeProps.HasFlag(GattProperty.SignedWrite))
+        {
+            result |= BluetoothCharacteristicProperties.SignedWrite;
+        }
+
+        if (nativeProps.HasFlag(GattProperty.ExtendedProps))
+        {
+            result |= BluetoothCharacteristicProperties.ExtendedProperties;
+        }
+
+        return result;
+    }
+
+    #endregion
+
+    #region Characteristic Permissions - Shared to Native
+
     /// <summary>
     ///     Converts the specified <see cref="BluetoothCharacteristicPermissions" /> to the corresponding Android <see cref="GattPermission" /> flags.
     /// </summary>
@@ -106,6 +169,63 @@ public static class GattMapping
         return result;
     }
 
+    #endregion
+
+    #region Characteristic Permissions - Native to Shared
+
+    /// <summary>
+    ///     Converts Android <see cref="GattPermission" /> flags to the corresponding <see cref="BluetoothCharacteristicPermissions" />.
+    /// </summary>
+    /// <param name="nativePerms">The Android GATT permission flags to convert.</param>
+    /// <returns>A <see cref="BluetoothCharacteristicPermissions" /> value representing the specified native permissions.</returns>
+    public static BluetoothCharacteristicPermissions ToSharedCharacteristicPermissions(this GattPermission nativePerms)
+    {
+        BluetoothCharacteristicPermissions result = 0;
+
+        // Read permissions
+        if (nativePerms.HasFlag(GattPermission.ReadEncryptedMitm))
+        {
+            result |= BluetoothCharacteristicPermissions.ReadAuthenticated;
+        }
+        else if (nativePerms.HasFlag(GattPermission.ReadEncrypted))
+        {
+            result |= BluetoothCharacteristicPermissions.ReadEncrypted;
+        }
+        else if (nativePerms.HasFlag(GattPermission.Read))
+        {
+            result |= BluetoothCharacteristicPermissions.Read;
+        }
+
+        // Write permissions
+        if (nativePerms.HasFlag(GattPermission.WriteSignedMitm))
+        {
+            result |= BluetoothCharacteristicPermissions.WriteSignedAuthenticated;
+        }
+        else if (nativePerms.HasFlag(GattPermission.WriteSigned))
+        {
+            result |= BluetoothCharacteristicPermissions.WriteSigned;
+        }
+
+        if (nativePerms.HasFlag(GattPermission.WriteEncryptedMitm))
+        {
+            result |= BluetoothCharacteristicPermissions.WriteAuthenticated;
+        }
+        else if (nativePerms.HasFlag(GattPermission.WriteEncrypted))
+        {
+            result |= BluetoothCharacteristicPermissions.WriteEncrypted;
+        }
+        else if (nativePerms.HasFlag(GattPermission.Write))
+        {
+            result |= BluetoothCharacteristicPermissions.Write;
+        }
+
+        return result;
+    }
+
+    #endregion
+
+    #region Descriptor Permissions - Shared to Native
+
     /// <summary>
     ///     Converts the specified <see cref="BluetoothDescriptorPermissions" /> to the corresponding Android <see cref="GattPermission" /> flags.
     /// </summary>
@@ -145,4 +265,51 @@ public static class GattMapping
 
         return result;
     }
+
+    #endregion
+
+    #region Descriptor Permissions - Native to Shared
+
+    /// <summary>
+    ///     Converts Android <see cref="GattPermission" /> flags to the corresponding <see cref="BluetoothDescriptorPermissions" />.
+    /// </summary>
+    /// <param name="nativePerms">The Android GATT permission flags to convert.</param>
+    /// <returns>A <see cref="BluetoothDescriptorPermissions" /> value representing the specified native permissions.</returns>
+    public static BluetoothDescriptorPermissions ToSharedDescriptorPermissions(this GattPermission nativePerms)
+    {
+        BluetoothDescriptorPermissions result = 0;
+
+        // Read permissions
+        if (nativePerms.HasFlag(GattPermission.ReadEncryptedMitm))
+        {
+            result |= BluetoothDescriptorPermissions.ReadAuthenticated;
+        }
+        else if (nativePerms.HasFlag(GattPermission.ReadEncrypted))
+        {
+            result |= BluetoothDescriptorPermissions.ReadEncrypted;
+        }
+        else if (nativePerms.HasFlag(GattPermission.Read))
+        {
+            result |= BluetoothDescriptorPermissions.Read;
+        }
+
+        // Write permissions
+        if (nativePerms.HasFlag(GattPermission.WriteEncryptedMitm))
+        {
+            result |= BluetoothDescriptorPermissions.WriteAuthenticated;
+        }
+        else if (nativePerms.HasFlag(GattPermission.WriteEncrypted))
+        {
+            result |= BluetoothDescriptorPermissions.WriteEncrypted;
+        }
+        else if (nativePerms.HasFlag(GattPermission.Write))
+        {
+            result |= BluetoothDescriptorPermissions.Write;
+        }
+
+        return result;
+    }
+
+    #endregion
 }
+
